954b6577af829983111fb08e37265797139af4b5
David Harrigan <dharrigan118@gmail.com>
2018-08-19 22:56:29 -0400

    remove comment for now

--- choboi.py
+++ choboi.py
@@ -1,18 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Supported commands:
-
-@choboi
-
-    herro -> parese
-
-    set LAN party <date> -> save LAN party date
-    when's LAN party? -> list LAN party dates
-
-    is <name> a cuck?
-
-    help -> print commands
-"""
 from choboi import config
 from choboi.bot import Bot
 
